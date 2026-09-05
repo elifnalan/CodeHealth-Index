3235b24e987fa21d6bb5b3495a7c3674c88b6772
elifnalan <emutlu1@cougars.ccis.edu>
2026-09-05 18:53:56 -0500

    Pydriller addition with rename-merge hnadling + exporing result into csv

---
+++ src/mine_repo.py
@@ -0,0 +1,72 @@
+from pydriller import Repository
+from collections import defaultdict
+import pandas as pd
+
+# Storing the file statistics in a dictionary with default values
+file_stats = defaultdict(lambda: {
+
+    #churn: number of lines added + number of lines deleted
+    "churn": 0,
+
+    "num_commits": 0,
+    "authors": set(),
+})
+
+
+# Repo used for this project:
+repo = Repository("../requests")
+
+
+# Traversing through the commits in the repository
+for commit in repo.traverse_commits():
+
+    # For every modified file in the commmit, storing the path, churn 
+    for mod in commit.modified_files:
+
+        # file renamed/moved, so merge its old history into the new path
+        if mod.change_type.name == "RENAME":
+            old_path = mod.old_path
+            new_path = mod.new_path
+
+            file_stats[new_path]["churn"] += file_stats[old_path]["churn"]
+            file_stats[new_path]["num_commits"] += file_stats[old_path]["num_commits"]
+            file_stats[new_path]["authors"].update(file_stats[old_path]["authors"])
+
+
+            file_stats[old_path] = {"churn": 0, "num_commits": 0, "authors": set()}  # Reset old path stats
+            
+            #print(f"RENAME detected: {old_path} -> {new_path}")
+        if not mod.new_path:
+            continue
+
+        file_stats[mod.new_path]["churn"] += mod.added_lines + mod.deleted_lines
+        file_stats[mod.new_path]["num_commits"] += 1
+
+        # becasue the username can change, storing the email of the author instead of the username
+        file_stats[mod.new_path]["authors"].add(commit.author.email)
+
+   
+for filepath in file_stats:
+    if "models.py" in filepath and file_stats[filepath]["num_commits"] > 0:
+        print(filepath, file_stats[filepath])
+
+print(f"\nTotal files tracked: {len(file_stats)}")
+
+
+#to insert the data into a database, we need to convert the data into a list of dictionaries
+rows = []
+for filepath, stats in file_stats.items():
+    if stats["num_commits"] > 0:
+
+        rows.append({
+            "filepath" : filepath,
+            "churn" : stats["churn"],
+            "num_commits" : stats["num_commits"],
+            "num_authors" : len(stats["authors"]),
+        })
+
+        print(f"File: {filepath}, Churn: {stats['churn']}, Commits: {stats['num_commits']}, Authors: {len(stats['authors'])}")
+
+#inserting the data into a csv file
+df = pd.DataFrame(rows)
+df.to_csv("raw_metrics.csv", index=False)
